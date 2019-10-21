0da0cb9152f95b77dffeefc2c7759941e8d75bb2
dw1a <dw1a@DW1A-15.inform.local>
2019-10-21 09:32:16 +0200

    Validando el radio, la base y la altura

--- Unidad_3/src/tanda1/Ejercicio02.java
+++ Unidad_3/src/tanda1/Ejercicio02.java
@@ -3,41 +3,45 @@ package tanda1;
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
-		//Programa que calcule y visualice el area de un circulo (c) o rectángulo (r)
+		// Programa que calcule y visualice el area de un circulo (c) o rectángulo (r)
 		char opcion;
 		double radio, altura, base, areaRectangulo, areaCirculo;
-		final double PI=3.1416;
-		
+		final double PI = 3.1416;
+
 		do {
 			System.out.println("Introduce c, para calcular el area de un círculo o r, para la de un rectángulo");
-			opcion=Consola.leeChar();
-		}
-		while (opcion!='c' && opcion!='C' && opcion!='r' && opcion!='R');
+			opcion = Consola.leeChar();
+		} while (opcion != 'c' && opcion != 'C' && opcion != 'r' && opcion != 'R');
 		switch (opcion) {
 		case 'c':
 		case 'C':
-			
-			System.out.println("Introduce el radio del circulo");
-			radio=Consola.leeDouble();
-			areaCirculo=PI*radio*radio;
-			System.out.println("El area del circulo es: "+ areaCirculo);
-			
+
+			do {
+				System.out.println("Introduce el radio del circulo");
+				radio = Consola.leeDouble();
+			} while (radio < 0);
+			areaCirculo = PI * radio * radio;
+			System.out.println("El area del circulo es: " + areaCirculo);
+
 			break;
 
 		case 'r':
 		case 'R':
-			
-			System.out.println("Introduce la base del rectángulo");
-			base=Consola.leeDouble();
-			System.out.println("Introduce la altura del rectángulo");
-			altura=Consola.leeDouble();
-			areaRectangulo=base*altura;
-			System.out.println("El area del rectángulo es: "+ areaRectangulo);
-			
-			
+
+			do {
+				System.out.println("Introduce la base del rectángulo");
+				base = Consola.leeDouble();
+			} while (base < 0);
+			do {
+				System.out.println("Introduce la altura del rectángulo");
+				altura = Consola.leeDouble();
+			} while (altura < 0);
+
+			areaRectangulo = base * altura;
+			System.out.println("El area del rectángulo es: " + areaRectangulo);
+
 			break;
-		}	
-		
+		}
 
 	}
 
