3b4aab0ab058567a87e542024db66af402e54b88
dw1a <dw1a@DW1A-15.inform.local>
2019-10-24 08:27:42 +0200

    Ejercicio09

--- Unidad_3/src/tanda1/Ejercicio09.java
+++ Unidad_3/src/tanda1/Ejercicio09.java
@@ -2,8 +2,42 @@ package tanda1;
 
 public class Ejercicio09 {
 
+	/**
+	 * Programa que determine si alguien es apto para un campeonato. Numero de
+	 * pruebas es una constante (10) debe cumplir al menos una de estas condiciones:
+	 * Ninguna prueba puede superar los 16 minutos, en al menos una el tiempo sera
+	 * menor a 10 minutos, el promedio de todas será menor o igual que 15
+	 * 
+	 */
 	public static void main(String[] args) {
-		//Programa que determine si alguien es apto para un campeonato. Numero de pruebas es una constante (10) debe cumplir al menos una de estas condiciones: Ninguna prueba puede superar los 16 minutos, en al menos una el tiempo sera menor a 10 minutos, el promedio de todas será menor o igual que 15
+
+		final int DIAS = 10;
+		boolean esApto = false;
+		double tiempoCarrera, promedioTiempos, totalTiempos = 0;
+		int contCarrera, contTiempoAlto = 0, contTiempoBajo = 0;
+
+		for (contCarrera = 1; contCarrera <= DIAS; contCarrera++) {
+			do {
+				System.out.println("Introduce el tiempo que te ha costado la carrera " + contCarrera);
+				tiempoCarrera = Consola.leeDouble();
+			} while (tiempoCarrera < 0);
+
+			if (tiempoCarrera > 16)
+				contTiempoAlto += 1;
+			if (tiempoCarrera < 10)
+				contTiempoBajo += 1;
+
+			totalTiempos += tiempoCarrera;
+
+		}
+		promedioTiempos=totalTiempos/DIAS;
+		
+		if(contTiempoAlto==0 || contTiempoBajo>0 || promedioTiempos<=15)
+			esApto=true;
+		if(esApto)
+			System.out.println("Puedes participar en el campeonato");
+		else
+			System.out.println("Este año no estás preparado para el campeonato, ánimo! sigue entrenando!");
 
 	}
 
--- Unidad_3/src/tanda1/Ejercicio10.java
+++ Unidad_3/src/tanda1/Ejercicio10.java
@@ -3,7 +3,7 @@ package tanda1;
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
