d21d702b48f8b849e81451aa29b5429ee4b24a52
dw1a <dw1a@DW1A-15.inform.local>
2019-10-24 13:09:24 +0200

    Cambiar Ejercicio 11 de paquete

---
+++ Unidad_3/src/tanda1/Ejercicio11.java
@@ -0,0 +1,44 @@
+package tanda1;
+
+
+public class Ejercicio11 {
+
+	public static void main(String[] args) {
+		/**
+		 * Programa que calcule la factorización de un numero
+		 */
+		int num, aux, cont;
+		boolean primerNumero=true;
+		
+		System.out.println("Introduce un número");
+		num=Consola.leeInt();
+		aux=num;
+		cont=2;
+		System.out.print(num + "=");
+		while(cont<=aux) {
+			if(aux%cont==0) {
+				while(aux%cont==0) {
+					if(primerNumero==true) {
+						System.out.print(cont);
+						primerNumero=false;
+						aux=aux/cont;
+						
+					}else {
+						System.out.print("*");
+						System.out.print(cont);
+						aux=aux/cont;
+					}
+					
+					
+						
+				}
+			}else {
+					cont++;
+					
+				}
+			}
+			
+		}
+	}
+
+
