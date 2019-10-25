1a877ab1c7fe9836764b2a49acfe0a14cd210c3c
dw1a <dw1a@DW1A-15.inform.local>
2019-10-25 09:36:12 +0200

    Ejercicio01 tanda2

---
+++ Unidad_3/src/tanda2/Ejercico01.java
@@ -0,0 +1,32 @@
+package tanda2;
+
+public class Ejercico01 {
+
+	public static void main(String[] args) {
+		// Calcular la suma y el producto de los numeros impares comprendidos entre dos
+		// constantes
+		final int LIMINF = 20, LIMSUP = 40;
+		int totalSuma, totalProducto, iteradorImpar;
+
+		/**
+		 * En vez de limpitarle a sumar uno para que sea impar, ya que conozco el valor,
+		 * hago este bucle por si se cambiase el valor de la constante
+		 **/
+		iteradorImpar = LIMINF;
+		do {
+			iteradorImpar += 1;
+		} while (iteradorImpar % 2 == 0);
+		totalProducto = 1;
+		totalSuma = 0;
+		while (iteradorImpar < LIMSUP) {
+			totalSuma += iteradorImpar;
+			totalProducto *= iteradorImpar;
+			iteradorImpar += 2;
+
+		}
+
+		System.out.println("El total de la suma es " + totalSuma + "\n El producto es " + totalProducto);
+
+	}
+
+}
