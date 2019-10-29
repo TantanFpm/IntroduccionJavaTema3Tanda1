0f2eb3b1b60d7ab8321368942a50d8e8f25382a9
dw1a <dw1a@DW1A-15.inform.local>
2019-10-29 13:03:27 +0100

    Tanda3 ejercicio02

--- Unidad_3/src/tanda3/Ejercicio02.java
+++ Unidad_3/src/tanda3/Ejercicio02.java
@@ -3,11 +3,31 @@ package tanda3;
 public class Ejercicio02 {
 		/**
 		 * Programa capaz de realizar la formula (n!)/(i!*(n-1)!) donde n e i se piden por teclado.
-		 *
+		 * Ambos deben ser positivos y n debe ser mayor que i.
 		 */
 	public static void main(String[] args) {
+		double n, i, factorialN=1, factorialI=1, factorialNMenos1=1, dividendo, resultado;
+		do {
+			System.out.println("Introduce un número");
+			n=Consola.leeDouble();
+		} while (n<=0);
+		do {
+			System.out.println("Introduce un número menor que el anterior");
+			i=Consola.leeDouble();
+		} while (i>=n || i<=0);
 		
-
+		for (int j=1; j<=n; j++) {
+			factorialN*=j;
+			if(j<=n-1)
+				factorialNMenos1*=j;
+			if(j<=i)
+				factorialI*=j;
+		}
+		
+		
+		dividendo=factorialI*factorialNMenos1;
+		resultado=factorialN/dividendo;
+		System.out.println("El resultado es: " + resultado);
 	}
 
 }
---
+++ Unidad_3/src/tanda3/Ejercicio03.java
@@ -0,0 +1,10 @@
+package tanda3;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
